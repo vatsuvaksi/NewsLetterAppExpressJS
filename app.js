975ac03602b6702ff35ffd8692846451b6555ed8
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-03-14 01:59:07 +0530

    Added push method for the form and parsed the html component through body parser to get the input from the user

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
-const request = require("request");
+const bodyParser = require("body-parser");
+//const request = require("request");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.listen(3000,function(){
 console.log("The server is running on the port: 3000");
+});
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+})
+app.post("/",function(req,res){
+    var name = req.body.name;
+    var email = req.body.Email;
+    console.log(name+ " " + email);
 });
