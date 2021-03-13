6bb2c125659e58f7f7d4ccf26c9966ae688004bf
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-03-14 01:26:22 +0530

    Added sign up page with styling using bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const request = require("request");
 app.listen(3000,function(){
 console.log("The server is running on the port: 3000");
 });
